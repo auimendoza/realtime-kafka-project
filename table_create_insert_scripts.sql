aa5f73584df9c68503587582d2a04f9f996da6cc
Vallath, Jayadev <jayadevvallath@gmail.com>
2016-05-28 08:47:41 +0000

    table create insert script for postgres

---
+++ table_create_insert_scripts.sql
@@ -0,0 +1,54 @@
+CREATE TABLE sales_rep (
+    sales_rep_id    char(5) CONSTRAINT firstkey PRIMARY KEY,
+    sales_rep_name  varchar(40)    NOT NULL,
+    sales_rep_territory varchar(5) NOT NULL,
+    daily_gallon_plan  integer     NOT NULL
+);
+
+INSERT INTO sales_rep VALUES ('1-001','John Smith','NE',500);
+INSERT INTO sales_rep VALUES ('1-002','Linda Errico','SE',1000);
+INSERT INTO sales_rep VALUES ('1-003','Ed Smith','NYC',3000);
+INSERT INTO sales_rep VALUES ('1-004','Dennis Brunzell','GL',2000);
+INSERT INTO sales_rep VALUES ('1-005','Carl Kowaski','WC',1000);
+INSERT INTO sales_rep VALUES ('1-006','Jung Lim','NYC',1000);
+INSERT INTO sales_rep VALUES ('1-007','Andy Jerkin','NE',500);
+INSERT INTO sales_rep VALUES ('1-008','Carla Pollak','WC',2000);
+INSERT INTO sales_rep VALUES ('1-009','Samson Smith','TX',4000);
+INSERT INTO sales_rep VALUES ('1-010','Lory Ann Curt','SE',3000);
+INSERT INTO sales_rep VALUES ('1-002','Linda Errico','SE'1000);
+
+CREATE TABLE territory (
+    territory_id    varchar(5) CONSTRAINT tkey PRIMARY KEY,
+    territory_name  varchar(40)    NOT NULL
+);
+
+INSERT INTO territory VALUES ('NE','Norh East');
+INSERT INTO territory VALUES ('SE','South East');
+INSERT INTO territory VALUES ('NYC','Newyork Metro');
+INSERT INTO territory VALUES ('GL','Great Lakes');
+INSERT INTO territory VALUES ('WC','West Coast');
+INSERT INTO territory VALUES ('TX','Texas');
+
+CREATE TABLE product (
+    product_id    varchar(5) CONSTRAINT pkey PRIMARY KEY,
+    gallons       decimal(7,2)    NOT NULL
+);
+
+INSERT INTO product VALUES ('N001',5);
+INSERT INTO product VALUES ('A001',5);
+INSERT INTO product VALUES ('C005',.5);
+INSERT INTO product VALUES ('M004',.25);
+INSERT INTO product VALUES ('N005',.5);
+
+CREATE TABLE transaction
+(order_date   date,
+ order_time   integer,
+ sales_rep_id char(5),
+ product_id   varchar(5),
+ unit_sold    integer);
+
+INSERT INTO transaction VALUES (current_date,1000,'1-001','N001',25);
+
+https://hub.docker.com/_/postgres/
+-- to remove a containter
+docker rm  container name
